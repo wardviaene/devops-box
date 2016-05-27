e83b3bf6e91ad305d73ce0b582b3743bfc6ce8c9
Edward Viaene <ward@in4it.io>
2016-05-27 17:53:54 +0200

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # create new ssh key
-mkdir -p ~/.ssh
-ssh-keygen -f ~/.ssh/mykey -N ''
+mkdir -p /home/ubuntu/.ssh
+ssh-keygen -f /home/ubuntu/.ssh/mykey -N ''
+chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 # install packages
 apt-get update
 apt-get -y install docker.io ansible python-pip unzip
-# install awscli
+# add docker privileges
+usermod -G docker ubuntu
+# install awscli and ebcli
 pip install awscli
+pip install awsebcli
 cd /usr/local/bin
 wget -q https://releases.hashicorp.com/terraform/0.6.16/terraform_0.6.16_linux_amd64.zip
 unzip terraform_0.6.16_linux_amd64.zip
