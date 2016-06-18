d94403e6bd6a0c14b597db9d1084164be3bd2c24
Edward Viaene <ward.viaene@bigdatapartnership.com>
2016-06-18 13:35:43 +0200

    Added manual install for pip

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,9 +5,14 @@ ssh-keygen -f /home/ubuntu/.ssh/mykey -N ''
 chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 # install packages
 apt-get update
-apt-get -y install docker.io ansible python-pip unzip
+apt-get -y install docker.io ansible unzip
 # add docker privileges
 usermod -G docker ubuntu
+# install pip
+wget -q https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+python3 get-pip.py
+rm -f get-pip.py
 # install awscli and ebcli
 pip install awscli
 pip install awsebcli
