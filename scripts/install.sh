b5a49d87edb2f77a2d4d848a545a060168d3713d
Edward Viaene <ward.viaene@gmail.com>
2018-06-16 08:45:56 +0200

    version bump

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,8 @@ if [ -e /etc/redhat-release ] ; then
   REDHAT_BASED=true
 fi
 
-TERRAFORM_VERSION="0.11.1"
-PACKER_VERSION="0.10.2"
+TERRAFORM_VERSION="0.11.7"
+PACKER_VERSION="1.2.4"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
 && mkdir -p /home/ubuntu/.ssh \
